08ad47058a1e3988d73f4274c6c87a9a120f5d31
Riccardo Balbo <os@rblb.it>
2025-08-25 18:53:46 +0200

    experimental async texture loading

--- jme3-core/src/main/java/com/jme3/texture/Image.java
+++ jme3-core/src/main/java/com/jme3/texture/Image.java
@@ -39,6 +39,7 @@ import com.jme3.export.Savable;
 import com.jme3.math.FastMath;
 import com.jme3.renderer.Caps;
 import com.jme3.renderer.Renderer;
+import com.jme3.system.Experimental;
 import com.jme3.texture.image.ColorSpace;
 import com.jme3.texture.image.LastTextureState;
 import com.jme3.util.BufferUtils;
@@ -612,6 +613,7 @@ public class Image extends NativeObject implements Savable /*, Cloneable*/ {
     protected boolean mipsWereGenerated = false;
     protected boolean needGeneratedMips = false;
     protected LastTextureState lastTextureState = new LastTextureState();
+    protected boolean async = false;
 
     /**
      * Internal use only.
@@ -1305,4 +1307,15 @@ public class Image extends NativeObject implements Savable /*, Cloneable*/ {
         }
     }
 
+    @Deprecated(forRemoval = false)
+    @Experimental
+    public void setAsync(boolean async) {
+        this.async = async;
+    }
+
+    @Deprecated(forRemoval = false)
+    @Experimental
+    public boolean isAsync() {
+        return async;
+    }
 }
